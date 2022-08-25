90d2076b3f359d653698c81d79e70691bffa412b
MoonHyeji <mhj5601@gmail.com>
2022-08-25 21:19:52 +0900

    첫번째 커밋

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template> 
+<div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoFooter></TodoFooter>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+</div>
+
+</template>
+<script>
+import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+
+
+
+export default {
+    components:{
+        'TodoHeader' : TodoHeader,
+        'TodoFooter' : TodoFooter,
+        'TodoInput' : TodoInput,
+        'TodoList' : TodoList
+    }
+
+}
+</script>
+<style>
+body{
+  text-align: center;
+  background-color: #F6F6F8;
+}
+input{
+  border-style: groove;
+  width: 200px;
+}
+button{
+  border-style: groove; /*groove 테두리가 창(canvas)에서 조각(파진)된 것처럼 보임*/
+  margin-left: 5px;
+}
+.shadow{
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <input type="text" v-model="newTodoItem">
+    <button @click="addTodo">할일 추가</button>
+</div>    
+</template>
+<script>
+export default {
+    data(){
+        return{
+            newTodoItem: ''
+        }
+    },
+    methods:{
+        addTodo(){
+            if(this.newTodoItem && this.newTodoItem !== ""){
+                var value = this.newTodoItem && this.newTodoItem.trim()
+                localStorage.setItem(value,value)
+                this.clearInput();
+        }   
+           // localStorage.setItem(this.newTodoItem, this.newTodoItem)
+        },
+        clearInput(){
+            this.newTodoItem = "";
+        }
+    }
+}
+</script>
+<style>
+</style>
